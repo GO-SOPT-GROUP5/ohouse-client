91d04499e097638b82da0e90b4d4a3405791ce91
lydiacho <sheecho0804@ewhain.net>
2023-05-17 01:31:30 +0900

    design : Explanation 이미지 5개 불러오기

---
+++ src/assets/image/index.ts
@@ -0,0 +1,5 @@
+export { ReactComponent as ImgExplanation1 } from './explanation_1.svg';
+export { ReactComponent as ImgExplanation2 } from './explanation_2.svg';
+export { ReactComponent as ImgExplanation3 } from './explanation_3.svg';
+export { ReactComponent as ImgExplanation4 } from './explanation_4.svg';
+export { ReactComponent as ImgExplanation5 } from './explanation_5.svg';
