f2f95ce0916b52e39ed33c80fb9460f250a6053b
lydiacho <sheecho0804@ewhain.net>
2023-05-18 02:49:30 +0900

    design : IcPlus svg 불러오기

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,3 +1,4 @@
 export { ReactComponent as IcLogo } from './ic_logo.svg';
 export { ReactComponent as IcLeftSmall } from './ic_left_small.svg';
 export { ReactComponent as IcSmallLine } from './ic_small_line.svg';
+export { ReactComponent as IcPlus } from './ic_plus.svg';
