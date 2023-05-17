fea286155f835bdac0b58c8de842ffac53e39548
lydiacho <sheecho0804@ewhain.net>
2023-05-18 02:50:32 +0900

    design : AddBox 구현

--- src/components/List/AddBox.tsx
+++ src/components/List/AddBox.tsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
+import { IcPlus } from "../../assets/icon";
+
 const AddBox = () => {
   return (
     <St.AddBoxWrapper>
-
+      <IcPlus/>
     </St.AddBoxWrapper>
   )
 }
@@ -11,13 +13,16 @@ const AddBox = () => {
 export default AddBox
 
 const St = {
-    AddBoxWrapper : styled.article`
-        
-        width: 39.3rem;
-        height: 42.3rem;
+  AddBoxWrapper : styled.article`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    width: 39.3rem;
+    height: 42.3rem;
 
-        background-color: ${({ theme }) => theme.colors.White};
-        border-radius: 0.9rem;
-        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
-    `
+    background-color: ${({ theme }) => theme.colors.White};
+    border-radius: 0.9rem;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+  `
 }
