6aedd48818bab06b88a551c91bacea6bafe10e2b
Yeonseo-Jo <jys073190@gmail.com>
2023-05-17 11:35:29 +0900

    feat : gnb 아이콘 추가

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,2 +1,10 @@
 export { ReactComponent as IcLogo } from './ic_logo.svg';
 export { ReactComponent as IcLeftSmall } from './ic_left_small.svg';
+
+// header-gnb icons
+export { ReactComponent as IcAlarm } from './ic_alarm.svg';
+export { ReactComponent as IcBookMark } from './ic_bookmark.svg';
+export { ReactComponent as IcCart } from './ic_cart.svg';
+export { ReactComponent as IcProfile } from './ic_profile.svg';
+export { ReactComponent as IcSearch } from './ic_search.svg';
+export { ReactComponent as BtnWrite } from './button_write.svg';
