354d7cef0aa05a4974fcd6f6da0b60b163314242
Yeonseo-Jo <jys073190@gmail.com>
2023-05-18 11:22:19 +0900

    feat : 홈 메뉴박스 ui 구현

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -3,3 +3,6 @@ export { ReactComponent as IcLeftSmall } from './ic_left_small.svg';
 
 //메뉴박스 아이콘들
 export { ReactComponent as IcHuman } from './ic_human.svg';
+//메뉴박스 청소서비스 아이콘
+
+export { ReactComponent as IcCleanService } from './ic_clean_service.svg';
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,80 @@
+import { styled } from 'styled-components';
+
+import { IcCleanService } from '../assets/icon';
 import MenuBox from '../components/Home/MenuBox';
 
 const HomePage = () => {
   return (
-    <>
-      홈 페이지입니다!
-      <MenuBox />
-    </>
+    <St.HomeWrapper>
+      <St.TopSection>
+        <MenuBox />
+        <MenuBox />
+        <MenuBox />
+        <MenuBox />
+        <MenuBox />
+        <MenuBox />
+      </St.TopSection>
+      <St.BottomSection>
+        <St.BottomTitle>
+          <h4>청소서비스</h4>
+          <IcCleanService />
+        </St.BottomTitle>
+        <St.Bottomcontent>
+          <MenuBox />
+          <MenuBox />
+          <MenuBox />
+        </St.Bottomcontent>
+      </St.BottomSection>
+    </St.HomeWrapper>
   );
 };
 
 export default HomePage;
+
+const St = {
+  HomeWrapper: styled.section`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding: 0 10rem;
+
+    background-color: ${({ theme }) => theme.colors.Grey100};
+  `,
+  TopSection: styled.section`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+
+    gap: 2rem;
+    padding: 8.5rem 0 3.9rem 0;
+    margin: auto;
+  `,
+  BottomSection: styled.section`
+    display: flex;
+    flex-direction: column;
+  `,
+  BottomTitle: styled.div`
+    display: flex;
+    margin-left: 10rem;
+
+    & > h4 {
+      padding-right: 0.4rem;
+
+      color: ${({ theme }) => theme.colors.Grey600};
+
+      ${({ theme }) => theme.fonts.Title4};
+    }
+    & > svg {
+      width: 4.6rem;
+      height: 3.2rem;
+    }
+  `,
+  Bottomcontent: styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+
+    padding: 1.6rem 0 4.6rem 0;
+    margin: auto;
+    gap: 2rem;
+  `,
+};
