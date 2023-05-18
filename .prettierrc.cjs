2877c17c309f69cb167be097c36bc183abed2576
Yeonseo-Jo <jys073190@gmail.com>
2023-05-18 14:48:21 +0900

    fix : 프리티어 파일 따옴표 잘못 바뀐거 적용 -> 처음에 develop에서 받아서서 .prettierrc 적용 안되어 있어서 발생한 문제

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,10 +2,10 @@ module.exports = {
   printWidth: 100,
   tabWidth: 2,
   singleQuote: true,
-  trailingComma: "all",
+  trailingComma: 'all',
   bracketSpacing: true,
   semi: true,
   useTabs: false,
-  arrowParens: "avoid",
-  endOfLine: "lf",
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
 };
