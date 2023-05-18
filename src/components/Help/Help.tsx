2ac966fabb47affd3464f05777290a8ef05ae508
lydiacho <sheecho0804@ewhain.net>
2023-05-18 22:30:16 +0900

    feat : carousel 내용 집어넣기

--- src/components/Help/Help.tsx
+++ src/components/Help/Help.tsx
@@ -1,7 +1,16 @@
+import "swiper/css";
+
 import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 import { IcLeft, IcRight } from "../../assets/icon";
-import { ImgExplanation1 } from "../../assets/image/index";
+import {
+  ImgExplanation1,
+  ImgExplanation2,
+  ImgExplanation3,
+  ImgExplanation4,
+  ImgExplanation5
+} from "../../assets/image/index";
 
 const Help = () => {
   return (
@@ -12,9 +21,35 @@ const Help = () => {
           <IcRight />
         </St.HelpIcon>
         <St.HelpContent>
-          <h1>집보며 하나하나 적기 힘드셨죠?<br/>이젠 쉽게 체크 하세요</h1>
-          <p>집 볼때 꼭 체크해야하는 핵심만 모아놨어요</p>
-          <ImgExplanation1/>
+          <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>
+            <h1>집보며 하나하나 적기 힘드셨죠?<br/>이젠 쉽게 체크 하세요</h1>
+            <p>집 볼때 꼭 체크해야하는 핵심만 모아놨어요</p>
+            <ImgExplanation1/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h1>기존의 기록 방식과 비교했을 때<br/>이런 점이 편해요</h1>
+            <ImgExplanation2/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h1>나에게 맞는 항목을<br/>선택해서 평가하세요</h1>
+            <ImgExplanation3/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h1>선택한 항목을 빠르게<br/>체크하세요</h1>
+            <p>체크뿐만 아니라 간단한 메모, 사진 첨부도 가능해요</p>
+            <ImgExplanation4/>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h1>체크 후 요약정보로<br/>평가 내역을 확인하세요</h1>
+            <ImgExplanation5/>
+          </SwiperSlide>
+        </Swiper>
           <button type='button'>새로운 매물 등록하기</button>
         </St.HelpContent>
       </St.HelpCard>
@@ -65,22 +100,21 @@ const St = {
     height: 100%;
     margin: 0rem 10.1rem;
 
-    & > h1 {
+    & h1 {
       margin-top: 4.3rem;
 
       ${({ theme }) => theme.fonts.Title1};
       color: ${({ theme }) => theme.colors.Grey600};
     }
 
-    & > p {
+    & p {
       margin-top: 1.2rem;
 
       ${({ theme }) => theme.fonts.Body3};
       color: ${({ theme }) => theme.colors.Grey400};
     }
 
-    & > svg {
-      width: 100%;
+    & svg {
       margin-top: 5rem;
     }
 
