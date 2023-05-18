702958d30f8ac7805fb9de35b508679e2f61dc04
Yeonseo-Jo <jys073190@gmail.com>
2023-05-18 10:56:36 +0900

    feat : 메뉴박스 컴포넌트 ui

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,2 +1,5 @@
 export { ReactComponent as IcLogo } from './ic_logo.svg';
 export { ReactComponent as IcLeftSmall } from './ic_left_small.svg';
+
+//메뉴박스 아이콘들
+export { ReactComponent as IcHuman } from './ic_human.svg';
--- src/components/Home/MenuBox.tsx
+++ src/components/Home/MenuBox.tsx
@@ -1,17 +1,74 @@
 import styled from 'styled-components';
 
+import { IcHuman } from '../../assets/icon';
+
 const MenuBox = () => {
-  return <St.MenuBoxWrapper>MenuBox 컴포넌트입니다!</St.MenuBoxWrapper>;
+  return (
+    <St.MenuBoxWrapper>
+      <St.MenuBoxHeader>
+        <St.MenuBoxTitle>인테리어시공</St.MenuBoxTitle>
+        <St.MenuBoxContent>
+          종합리모델링, <br />
+          부분시공,카페공간 등 <br />
+          분야별 업체찾기
+        </St.MenuBoxContent>
+      </St.MenuBoxHeader>
+      <St.MenuBoxBottom>
+        <St.MenuBoxIcon>
+          <IcHuman />
+        </St.MenuBoxIcon>
+      </St.MenuBoxBottom>
+    </St.MenuBoxWrapper>
+  );
 };
 
 export default MenuBox;
 
 const St = {
   MenuBoxWrapper: styled.article`
+    position: relative;
+
     width: 39.2rem;
+    height: 18rem;
+    padding: 1.8rem;
 
     background-color: ${({ theme }) => theme.colors.White};
-    color: ${({ theme }) => theme.colors.Grey500};
+
+    box-shadow: 0 0.25rem 0.94rem rgba(0, 0, 0, 0.1);
+    border-radius: 1.2rem;
+  `,
+  MenuBoxHeader: styled.header`
+    display: flex;
+    flex-direction: column;
+  `,
+  MenuBoxTitle: styled.h3`
+    padding: 0.1rem 0 0.6rem 0;
+
     ${({ theme }) => theme.fonts.Title3};
+    color: ${({ theme }) => theme.colors.Grey500};
+  `,
+  MenuBoxContent: styled.p`
+    min-width: 10.8rem;
+    max-width: 18.8rem;
+
+    ${({ theme }) => theme.fonts.Body6};
+    color: ${({ theme }) => theme.colors.Grey400};
+
+    white-space: pre-line;
+    word-break: keep-all;
+  `,
+  MenuBoxBottom: styled.div`
+    padding-top: 0.3rem;
+  `,
+  MenuBoxIcon: styled.div`
+    & > svg {
+      position: absolute;
+      right: 0;
+      bottom: 0;
+
+      width: 4.2rem;
+      height: 4.2rem;
+      margin: 0 1.8rem 1.8rem 0;
+    }
   `,
 };
