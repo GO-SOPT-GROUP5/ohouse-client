ac1126b640262829c65a3d30f1995e6571f95f6c
Yeonseo-Jo <jys073190@gmail.com>
2023-05-22 11:10:38 +0900

    chore : 테스트용 홈페이지 코드 제거

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,3 @@
-import { styled } from 'styled-components';
-
 import MenuBox from '../components/Home/MenuBox';
 
 const HomePage = () => {
@@ -7,20 +5,8 @@ const HomePage = () => {
     <>
       홈 페이지입니다!
       <MenuBox />
-      <Test>테스트용 호마ㅕㄴ !~~~</Test>
-      <Test2>테스트용 호마ㅕㄴ !~~~</Test2>
     </>
   );
 };
 
 export default HomePage;
-
-const Test = styled.div`
-  background-color: pink;
-  height: 100vh;
-`;
-
-const Test2 = styled.div`
-  background-color: yellow;
-  height: 100vh;
-`;
