96cace1630f1c5e20840d05f01adeaafbef2dd1a
Yeonseo-Jo <jys073190@gmail.com>
2023-05-22 10:52:23 +0900

    feat : 푸터 하단고정(스크롤시에도 고정될수 있도록)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,5 @@
+import { styled } from 'styled-components';
+
 import MenuBox from '../components/Home/MenuBox';
 
 const HomePage = () => {
@@ -5,8 +7,20 @@ const HomePage = () => {
     <>
       홈 페이지입니다!
       <MenuBox />
+      <Test>테스트용 호마ㅕㄴ !~~~</Test>
+      <Test2>테스트용 호마ㅕㄴ !~~~</Test2>
     </>
   );
 };
 
 export default HomePage;
+
+const Test = styled.div`
+  background-color: pink;
+  height: 100vh;
+`;
+
+const Test2 = styled.div`
+  background-color: yellow;
+  height: 100vh;
+`;
--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,16 +1,25 @@
 import { Outlet } from 'react-router-dom';
+import { styled } from 'styled-components';
 
 import Footer from '../components/common/Footer';
 import Header from '../components/common/Header';
 
 const MainLayout = () => {
   return (
-    <>
+    <St.mainLayoutWrapper>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </St.mainLayoutWrapper>
   );
 };
 
 export default MainLayout;
+
+const St = {
+  mainLayoutWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  `,
+};
