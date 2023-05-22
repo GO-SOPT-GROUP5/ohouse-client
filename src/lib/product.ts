84e2d772a34dbaaa2515c2773c76fca340430474
lydiacho <sheecho0804@ewhain.net>
2023-05-22 22:00:02 +0900

    feat : lib/product.ts getProductData API 통신  구현

--- src/lib/product.ts
+++ src/lib/product.ts
@@ -1,9 +1,17 @@
-import { client } from './axios';
+import {
+  OHouseResponse,
+  productRequest,
+  productResponse
+} from "../types/product";
+import { client } from "./axios";
 
-export const getProductData = async (productid: number) => {
+export const getProductData = async ({flag, order, page, size} : productRequest) => {
   try {
-    const { data } = await client.get(`/${productid}`);
-    return data.data;
+    const {data} = flag ? 
+      await client.get<OHouseResponse<productResponse>>(`/list?flag=${flag}&order=${order}&page=${page}&size=${size}`) : 
+      await client.get<OHouseResponse<productResponse>>(`/list?order=${order}&page=${page}&size=${size}`);
+  
+      return data.data.productList;
   } catch (err) {
     console.error(err);
   }
