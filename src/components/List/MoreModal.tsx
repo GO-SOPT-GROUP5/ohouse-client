ae2500544966993b63e4ce3ac142d1e113e95864
lydiacho <sheecho0804@ewhain.net>
2023-05-23 23:10:51 +0900

    design : MoreModal.tsx 태그 UI

--- src/components/List/MoreModal.tsx
+++ src/components/List/MoreModal.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import styled from "styled-components";
 
-const MoreModal = () => {
+export interface ModalProps {
+    isShowing : boolean;
+}
+
+const MoreModal = ({ isShowing }: ModalProps) => {
+    
+
   return (
-    <div>MoreModal</div>
+    <St.ListModalWrapper>
+        <St.ListModal>
+            <p>더보기</p>
+            <i>X</i>
+            <button type="button">수정하기</button>
+            <button type="button">삭제하기</button>
+        </St.ListModal>
+    </St.ListModalWrapper>
   )
 }
 
 export default MoreModal
+
+const St = {
+    ListModalWrapper : styled.section`
+    
+    `,
+    ListModal : styled.article`
+        
+    `,
+
+}
