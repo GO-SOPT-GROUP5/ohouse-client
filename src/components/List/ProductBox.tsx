53a63fce72ea7e9d3bf811119181ee25266c774e
lydiacho <sheecho0804@ewhain.net>
2023-05-23 23:23:48 +0900

    feat : ProductBox에서 toggle 불러오기

--- src/components/List/ProductBox.tsx
+++ src/components/List/ProductBox.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { IcListStar } from "../../assets/icon";
 import { ImgRoom1 } from "../../assets/image";
 
-const ProductBox = () => {
+const ProductBox = ({toggle}) => {
   
   return (
   <St.ProductBoxWrapper>
@@ -25,7 +25,7 @@ const ProductBox = () => {
     </St.ProductStar>
     <St.ProductButtons>
       <button type="button">체크리스트 내역 보기</button>
-      <button type="button">...</button>
+      <button type="button" onClick={toggle}>...</button>
     </St.ProductButtons>
   </St.ProductBoxWrapper>
   );
