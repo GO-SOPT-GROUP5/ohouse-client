99dd025019fbbb6ee52a9b908fd859a1d5265297
lydiacho <sheecho0804@ewhain.net>
2023-05-24 23:55:29 +0900

    init : .env 생성

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const client = axios.create({
-  baseURL: import.meta.env.BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-type': 'application/json',
   },
