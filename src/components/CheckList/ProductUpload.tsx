4f59d0e4adc4c490f7ff594cada4f37a8471cf88
Yeonseo-Jo <jys073190@gmail.com>
2023-05-25 15:24:02 +0900

    feat : 리코일로 매물 정보 관리

--- src/components/CheckList/ProductUpload.tsx
+++ src/components/CheckList/ProductUpload.tsx
@@ -1,17 +1,24 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { useRecoilValue, useResetRecoilState, useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
 
 import { IcCamera, IcEdit, IcStar, IcStarFilled } from '../../assets/icon';
 import useModal from '../../hooks/useModal';
+import { productDataState } from '../../recoil/atom';
 import ProductEditModal from './ProductEditModal';
 
 const ProductUpload = () => {
   const { isShowing, toggle } = useModal();
-  const [title, setTitle] = useState<string>('2023.01.10 12:11 등록매물');
-  // 주소
-  const [address, setAddress] = useState<string>('');
-  const [dong, setDong] = useState<string>();
-  const [ho, setHo] = useState<string>();
+  // const [title, setTitle] = useState<string>('2023.01.10 12:11 등록매물');
+  // // 주소
+  // const [address, setAddress] = useState<string>('');
+  // const [dong, setDong] = useState<string>();
+  // const [ho, setHo] = useState<string>();
+
+  const setProduct = useSetRecoilState(productDataState);
+  const { title, address, dong, ho, state, price, size, description } =
+    useRecoilValue(productDataState);
+
   // 별점
   const [grade, setGrade] = useState(0);
   const [starClicked, setStarClicked] = useState([false, false, false, false, false]);
@@ -22,9 +29,16 @@ const ProductUpload = () => {
   // 파일 업로드
   const ref = useRef<HTMLInputElement>(null);
   const [URLThumbnail, setURLThumbnail] = useState<string>('');
-  const [imageUrl, setImageUrl] = useState<string>(''); // 서버한테 보내줄때는 빈값으로
+  // const [imageUrl, setImageUrl] = useState<string>(''); // 서버한테 보내줄때는 빈값으로
   // 태그
-  const [tags, setTags] = useState<string[]>(['월세', '1000/60', '30평']);
+  // const [tags, setTags] = useState<string[]>(['월세', '1000/60', '30평']);
+
+  const product = useRecoilValue(productDataState);
+
+  useEffect(() => {
+    console.log('starClicked', starClicked);
+    console.log('grade', grade);
+  }, [grade]);
 
   const createImageURL = (fileBlob: Blob | MediaSource) => {
     if (URLThumbnail) URL.revokeObjectURL(URLThumbnail);
@@ -45,13 +59,16 @@ const ProductUpload = () => {
   };
 
   const handleStarClick = (index: number) => {
-    let clickStates = [...starClicked];
+    const clickStates = [...starClicked];
     for (let i = 0; i < 5; i++) {
       clickStates[i] = i <= index ? true : false;
     }
     setStarClicked(clickStates);
     setGrade(starClicked.filter(Boolean).length); // 서버에게 보낼 별 갯수
   };
+
+  // console.log('clickStates: ', starClicked);
+  // console.log('grade:', grade);
 
   const handleCommentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setComment(e.target.value);
@@ -61,10 +78,19 @@ const ProductUpload = () => {
     if (comment) {
       setShowCommentInput(false);
     }
+    setProduct(prev => ({
+      ...prev,
+      grade: grade,
+      description: comment,
+      image: '',
+    }));
   };
 
   return (
     <St.ProductUploadWrapper>
+      <button type="button" onClick={useResetRecoilState(productDataState)}>
+        저장하기 테스트
+      </button>
       <St.ProductName>{title}</St.ProductName>
       <St.Address>
         {address ? (
@@ -73,7 +99,7 @@ const ProductUpload = () => {
             {dong && ho ? `${dong}동 ${ho}호` : ''}
           </>
         ) : (
-          <St.DefaultAddress>주소를 등록해주세요</St.DefaultAddress>
+          <St.DefaultAddress>{address ? address : '주소를 등록해주세요'}</St.DefaultAddress>
         )}
       </St.Address>
       <St.EditBtn type="button" onClick={toggle}>
@@ -100,9 +126,9 @@ const ProductUpload = () => {
         </St.UploadPicture>
         <St.ProductDetail>
           <St.ProductTag>
-            {tags.map((tag, index) => (
-              <li key={index}>{tag}</li>
-            ))}
+            {state && <li>{state}</li>}
+            {state && price && <li>{price}</li>}
+            {state && size && <li>{size}평</li>}
           </St.ProductTag>
           <St.Grade>
             {array.map(el => (
@@ -120,7 +146,7 @@ const ProductUpload = () => {
                 onChange={handleCommentChange}
               ></St.Description>
             ) : (
-              <St.CommentText>{comment}</St.CommentText>
+              <St.CommentText>{description}</St.CommentText>
             )}
           </St.DescriptionWrapper>
           <St.SaveBtnWrapper>
